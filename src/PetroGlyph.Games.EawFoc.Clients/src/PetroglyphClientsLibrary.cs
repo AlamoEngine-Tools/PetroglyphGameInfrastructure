a8a23964b9697d2bcde0c2a1a2b2176beceafcc1
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-10-17 15:47:05 +0200

    add init code and remove test

---
+++ src/PetroGlyph.Games.EawFoc.Clients/src/PetroglyphClientsLibrary.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetroGlyph.Games.EawFoc.Clients.Processes;
+
+namespace PetroGlyph.Games.EawFoc.Clients
+{
+    /// <summary>
+    /// Provides initialization routines for this library.
+    /// </summary>
+    public class PetroglyphClientsLibrary
+    {
+        /// <summary>
+        /// Adds services provided by this library to the given <paramref name="serviceCollection"/>
+        /// so that the library can be used in client applications. 
+        /// </summary>
+        /// <param name="serviceCollection">The service collection to be filled.</param>
+        public static void InitializeLibraryWithDefaultServices(IServiceCollection serviceCollection)
+        {
+            // Singletons
+            serviceCollection.AddTransient<IProcessHelper>(_ => new ProcessHelper());
+        }
+    }
+}
