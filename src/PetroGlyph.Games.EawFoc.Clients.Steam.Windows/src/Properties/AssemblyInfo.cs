527d7e1ebf382353b32672d0ae32dea8f34e0ec9
AnakinRaW <anakin@gmx-topmail.de>
2024-03-15 11:34:02 +0100

    remove leftovers

--- src/PetroGlyph.Games.EawFoc.Clients.Steam.Windows/src/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("PG.StarWarsGame.Infrastructure.Clients.Steam.Windows.Test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
