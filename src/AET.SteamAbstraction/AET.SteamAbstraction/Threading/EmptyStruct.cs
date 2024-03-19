7f788db42005c3ab83f7a68559fb28ff83d35fe0
AnakinRaW <anakin@gmx-topmail.de>
2024-03-19 12:28:49 +0100

    update deps and enable tests partially

--- src/AET.SteamAbstraction/AET.SteamAbstraction/Threading/EmptyStruct.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AET.SteamAbstraction.Threading;
-
-// From https://github.com/microsoft/vs-threading
-internal readonly struct EmptyStruct
-{
-    internal static EmptyStruct Instance => default;
-}
--- src/AET.SteamAbstraction/AET.SteamAbstraction/Wrappers/WindowsSteamWrapper.cs
+++ src/AET.SteamAbstraction/AET.SteamAbstraction/Wrappers/WindowsSteamWrapper.cs
@@ -1,51 +1,52 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using AET.SteamAbstraction.Threading;
+using AnakinRaW.CommonUtilities.Registry.Windows;
 
 namespace AET.SteamAbstraction;
 
 internal class WindowsSteamWrapper(IServiceProvider serviceProvider) : SteamWrapper(serviceProvider)
 {
-    protected override Task WaitSteamUserLoggedInAsync(CancellationToken token)
+    protected override async Task WaitSteamUserLoggedInAsync(CancellationToken token)
     {
-        throw new NotImplementedException();
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        token.ThrowIfCancellationRequested();
+        if (IsUserLoggedIn)
+            return;
 
-        //token.ThrowIfCancellationRequested();
-        //if (IsUserLoggedIn)
-        //    return;
-
-        //while (!IsUserLoggedIn)
-        //{
-        //    token.ThrowIfCancellationRequested();
-        //    var processKey = Registry.ActiveProcessKey;
-        //    if (processKey is null)
-        //        return;
-        //    if (processKey is not WindowsRegistryKey windowsRegistryKey)
-        //        throw new InvalidOperationException("Expected Windows registry Key");
+        while (!IsUserLoggedIn)
+        {
+            token.ThrowIfCancellationRequested();
+            var processKey = Registry.ActiveProcessKey;
+            if (processKey is null)
+                return;
+            if (processKey is not WindowsRegistryKey windowsRegistryKey)
+                throw new InvalidOperationException("Expected Windows registry Key");
 
 
-        //    await windowsRegistryKey.WindowsKey.WaitForChangeAsync(false, RegistryChangeNotificationFilters.Value, token);
-        //}
+            await windowsRegistryKey.WindowsKey.WaitForChangeAsync(false, RegistryChangeNotificationFilters.Value, token);
+        }
     }
 
-    protected override Task WaitSteamRunningAsync(CancellationToken token)
+    protected override async Task WaitSteamRunningAsync(CancellationToken token)
     {
-        throw new NotImplementedException();
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        token.ThrowIfCancellationRequested();
+        if (IsRunning)
+            return;
 
-        //token.ThrowIfCancellationRequested();
-        //if (IsRunning)
-        //    return;
-
-        //while (!IsRunning)
-        //{
-        //    token.ThrowIfCancellationRequested();
-        //    var processKey = Registry.ActiveProcessKey;
-        //    if (processKey is null)
-        //        return;
-        //    if (processKey is not WindowsRegistryKey windowsRegistryKey)
-        //        throw new InvalidOperationException("Expected Windows registry Key");
-        //    await windowsRegistryKey.WindowsKey.WaitForChangeAsync(false, RegistryChangeNotificationFilters.Value, token);
-        //}
+        while (!IsRunning)
+        {
+            token.ThrowIfCancellationRequested();
+            var processKey = Registry.ActiveProcessKey;
+            if (processKey is null)
+                return;
+            if (processKey is not WindowsRegistryKey windowsRegistryKey)
+                throw new InvalidOperationException("Expected Windows registry Key");
+            await windowsRegistryKey.WindowsKey.WaitForChangeAsync(false, RegistryChangeNotificationFilters.Value, token);
+        }
     }
 }
