659c39ba63c14986e49da46035f86c94fffd5b31
AnakinRaW <anakin@gmx-topmail.de>
2025-01-01 18:03:52 +0100

    edit proj

--- src/PG.StarWarsGame.Infrastructure.Clients.Steam/Properties/AssemblyInfo.cs
+++ src/PG.StarWarsGame.Infrastructure.Clients.Steam/Properties/AssemblyInfo.cs
@@ -1,8 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("PG.StarWarsGame.Infrastructure.Clients.Steam")]
-[assembly: InternalsVisibleTo("PG.StarWarsGame.Infrastructure.Clients.Steam.Windows")]
-[assembly: InternalsVisibleTo("PG.StarWarsGame.Infrastructure.Clients.Test")]
 [assembly: InternalsVisibleTo("PG.StarWarsGame.Infrastructure.Clients.Steam.Test")]
-[assembly: InternalsVisibleTo("PG.StarWarsGame.Infrastructure.Clients.Steam.Windows.Test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
