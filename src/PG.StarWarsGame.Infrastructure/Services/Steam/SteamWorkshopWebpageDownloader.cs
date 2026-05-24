473dd4bb744d01eeae95349ab4876dfd08d9a63d
AnakinRaW <anakin@gmx-topmail.de>
2026-05-24 23:38:17 +0200

    Simplify `SteamWorkshopWebpageDownloader` by removing retry logic and backoff handling.

--- src/PG.StarWarsGame.Infrastructure/Services/Steam/SteamWorkshopWebpageDownloader.cs
+++ src/PG.StarWarsGame.Infrastructure/Services/Steam/SteamWorkshopWebpageDownloader.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
@@ -10,9 +9,6 @@ namespace PG.StarWarsGame.Infrastructure.Services.Steam;
 internal class SteamWorkshopWebpageDownloader : ISteamWorkshopWebpageDownloader
 {
     private const string SteamWorkshopsBaseUrl = "https://steamcommunity.com/sharedfiles/filedetails/?";
-    private const int MaxRetries = 4;
-    private static readonly TimeSpan InitialBackoff = TimeSpan.FromSeconds(1);
-    private static readonly TimeSpan MaxBackoff = TimeSpan.FromSeconds(30);
 
     public async Task<HtmlDocument> GetSteamWorkshopsPageHtmlAsync(ulong workshopId, CultureInfo? culture)
     {
@@ -24,45 +20,9 @@ internal class SteamWorkshopWebpageDownloader : ISteamWorkshopWebpageDownloader
 
         var address = $"{SteamWorkshopsBaseUrl}{queryString}";
         using var client = new HttpClient();
-
-        var backoff = InitialBackoff;
-        for (var attempt = 0; ; attempt++)
-        {
-            using var response = await client.GetAsync(address).ConfigureAwait(false);
-
-            if ((int)response.StatusCode != 429)
-            {
-                response.EnsureSuccessStatusCode();
-                var reply = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(reply);
-                return htmlDocument;
-            }
-
-            if (attempt >= MaxRetries)
-                response.EnsureSuccessStatusCode();
-
-            var wait = GetRetryAfter(response) ?? backoff;
-            if (wait > MaxBackoff)
-                wait = MaxBackoff;
-            await Task.Delay(wait).ConfigureAwait(false);
-
-            backoff = TimeSpan.FromTicks(Math.Min(backoff.Ticks * 2, MaxBackoff.Ticks));
-        }
-    }
-
-    private static TimeSpan? GetRetryAfter(HttpResponseMessage response)
-    {
-        var header = response.Headers.RetryAfter;
-        if (header is null)
-            return null;
-        if (header.Delta is { } delta && delta > TimeSpan.Zero)
-            return delta;
-        if (header.Date is { } when)
-        {
-            var delay = when - DateTimeOffset.UtcNow;
-            return delay > TimeSpan.Zero ? delay : null;
-        }
-        return null;
+        var reply = await client.GetStringAsync(address);
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(reply);
+        return htmlDocument;
     }
 }
